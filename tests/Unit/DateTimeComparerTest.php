1971c3c68a8dde6c47a6329f6993b8cdeca1421a
AlexArdalich <alex@ardalich.com>
2020-02-17 23:19:12 +1030

    add tests to check if datetimes are in reverse order

--- tests/Unit/DateTimeComparerTest.php
+++ tests/Unit/DateTimeComparerTest.php
@@ -20,6 +20,17 @@ class DateTimeComparerTest extends TestCase
     }
 
     /** @test */
+    public function can_find_number_of_days_between_two_reversed_datetimes()
+    {
+        $startDateTime = '2020-01-01 09:00';
+        $endDateTime = '2020-01-07 09:00';
+
+        $comparer = new DateTimeComparer($startDateTime, $endDateTime);
+
+        $this->assertEquals(6, $comparer->daysBetween());
+    }
+
+    /** @test */
     public function can_find_number_of_weekdays_between_two_datetimes()
     {
         $startDateTime = '2020-01-01 09:00';
@@ -31,7 +42,29 @@ class DateTimeComparerTest extends TestCase
     }
 
     /** @test */
+    public function can_find_number_of_weekdays_between_two_reversed_datetimes()
+    {
+        $startDateTime = '2020-01-01 09:00';
+        $endDateTime = '2020-01-07 09:00';
+
+        $comparer = new DateTimeComparer($startDateTime, $endDateTime);
+
+        $this->assertEquals(4, $comparer->weekDaysBetween());
+    }
+
+    /** @test */
     public function can_find_number_of_complete_weeks_between_two_datetimes()
+    {
+        $startDateTime = '2020-01-01 09:00';
+        $endDateTime = '2020-01-22 09:00';
+
+        $comparer = new DateTimeComparer($startDateTime, $endDateTime);
+
+        $this->assertEquals(3, $comparer->completeWeeksBetween());
+    }
+
+    /** @test */
+    public function can_find_number_of_complete_weeks_between_two_reversed_datetimes()
     {
         $startDateTime = '2020-01-01 09:00';
         $endDateTime = '2020-01-22 09:00';
