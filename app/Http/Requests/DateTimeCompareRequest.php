e67061fe7f541505e54b4cdad5a3674a4eb35833
AlexArdalich <alex@ardalich.com>
2020-02-18 11:37:14 +1030

    revert validation check for date

--- app/Http/Requests/DateTimeCompareRequest.php
+++ app/Http/Requests/DateTimeCompareRequest.php
@@ -28,8 +28,8 @@ class DateTimeCompareRequest extends FormRequest
     public function rules()
     {
         return [
-            'startDateTime' => 'required|date_format',
-            'endDateTime' => 'required|date_format',
+            'startDateTime' => 'required|date',
+            'endDateTime' => 'required|date',
             'startDateTimeZone' => 'timezone',
             'endDateTimeZone' => 'timezone',
             'resultFormat' => 'in:seconds,minutes,hours,years'
